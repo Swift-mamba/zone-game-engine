a46cf8b8db68adfa19e05db655ceff4245ff157f
Swift-mamba <151572703+Swift-mamba@users.noreply.github.com>
2023-11-27 20:13:20 -0500

    Update Zgraphics.py

--- MainCore/Zgraphics.py
+++ MainCore/Zgraphics.py
@@ -5,8 +5,8 @@ class ZN_GraphicsContext():
         self.canvas = 0
     def Create(self,bindto):
         self.canvas = bindto
-    def Drawtext(x,y,Text,color):
+    def Drawtext(self,x,y,Text,color):
         c = self.canvas
-        c.create_text(x, y, text=Text, fill=color,font = "Verdana")
+        c.create_text(x, y, text=Text, fill=color)
     def DrawRect(xpo,ypo,xpt,ypt,color):
         self.canvas.create_rectangle(xpo, ypo, xpt, ypt, fill=color)
