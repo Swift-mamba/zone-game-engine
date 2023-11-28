d542d9d1695e2f2bd7924174e3123ae56c7cb1ba
Swift-mamba <151572703+Swift-mamba@users.noreply.github.com>
2023-11-27 19:53:34 -0500

    add tick support to  Zdefinitions.py

--- MainCore/Zdefinitions.py
+++ MainCore/Zdefinitions.py
@@ -4,8 +4,15 @@
 " Also colors and stuff like it "
 """""""""""""""""""""""""""""""""
 import keyboard as kb
+
+global ticks
+ticks = 0
 def Keypressed(argone):
     OOO = kb.getpressed(argone)
     return(OOO)
 def ZN_Deinit():
     quit()
+def ZN_Tick():
+    ticks += 1
+def GetTicks():
+    return(ticks)
