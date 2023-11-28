065af6a526f0dbc2f5802e536769f55400c1bc06
Swift-mamba <151572703+Swift-mamba@users.noreply.github.com>
2023-11-27 21:02:41 -0500

    Update window.py

--- MainCore/window.py
+++ MainCore/window.py
@@ -4,11 +4,14 @@ class ZN_window():
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Zone game engine")
+        print("Create window")
         self.root.geometry("800x450")
+        print("size window(800x450)")
         self.canvas = tk.Canvas(self.root, width=800, height=450)
         self.canvas.pack(fill = tk.BOTH)
         self.root.resizable(0,0)
         self.canvas.create_rectangle(0, 0, 800, 450, fill='#00003c')
+        print("make window drawable")
     def Update(self):
         self.root.update()
     def Mainloop(self):
